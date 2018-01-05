a9696dd125150376918842366bb9b7b11117d33e
gdufeZLYL <18027894043@163.com>
2018-01-05 19:05:02 +0800

    平台首页-Springboot本地文件服务器,thymeleaf图片显示

---
+++ src/main/java/com/squirrel/config/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,24 @@
+package com.squirrel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+    private static final String UPLOAD_IMAGE_PATH = "file:D:/IdeaProjects/data/springboot-squirrel/upload/images/";
+
+    /**
+     * 配置静态访问资源
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //自定义项目内目录
+        //registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
+        //指向外部目录
+        registry.addResourceHandler("/upload/**").addResourceLocations(UPLOAD_IMAGE_PATH);
+        super.addResourceHandlers(registry);
+    }
+}
